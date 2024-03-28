058e2e063682285fecff51880e7814e10f79579c
jemis-lakhani <jemislakhani.tech@gmail.com>
2024-03-28 09:55:08 +0530

    - UI fix added.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,7 +3,7 @@ export const cardBG = {
   154914: "bg-gradient-to-r to-indigo-800 from-red-500", // Baseball
   48242: "bg-gradient-to-r to-indigo-800 from-orange-500", // Basketball
   131506: "bg-gradient-to-r to-indigo-800 from-purple-500", // American Football
-  687890: "bg-gradient-to-r to-sky-950 to-fromeen-500", // E-games
+  687890: "bg-gradient-to-r to-sky-950 to from-green-500", // E-games
   35232: "bg-gradient-to-r to-indigo-800 from-gray-500", // Hockey
   154830: "bg-gradient-to-r to-indigo-800 from-yellow-500 ", // Volleybal
 };
