4dbda78aea0d6f22b95b9a57da86706d82645f6b
jemis-lakhani <jemislakhani.tech@gmail.com>
2024-04-20 18:21:39 +0530

    Code refactored.

--- src/modules/fixture/FixtureMarket.jsx
+++ src/modules/fixture/FixtureMarket.jsx
@@ -79,46 +79,6 @@ const FixtureMarket = () => {
       : "";
   };
 
-  useEffect(() => {
-    // Load the custom script dynamically
-    const script = document.createElement("script");
-    script.innerHTML = `
-      !(function(){
-        var d="STATSCOREWidgetsEmbederScript";
-        if(!window.document.getElementById(d)){
-          window.STATSCOREWidgets={};
-          window.STATSCOREWidgets.onLoadCallbacks=[];
-          window.STATSCOREWidgets.onLoad=function(d){
-            window.STATSCOREWidgets.onLoadCallbacks.push(d)
-          };
-          var n=window.document.createElement("script");
-          n.src="https://wgt-s3-cdn.statscore.com/bundle/Embeder.js";
-          n.async=!0;
-          n.id=d;
-          n.addEventListener("error",function(d){
-            for(var n=0;n<window.STATSCOREWidgets.onLoadCallbacks.length;n++)
-              window.STATSCOREWidgets.onLoadCallbacks[n](d)
-          });
-          window.document.body.appendChild(n)
-        }
-      }());
-      window.STATSCOREWidgets.onLoad(function(e){
-        if(e){
-          console.error(e);
-          return;
-        }
-        new window.STATSCOREWidgets.Widget(document.getElementById("STATSCOREWidget17135898724143"),"66234e314c6a5387703a7332",{"language":"en","eventId":"m:${fixtureId}"},{loader:{enabled:true}});
-      });
-    `;
-
-    document.body.appendChild(script);
-
-    // Clean up function to remove the script when the component unmounts
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, [fixtureId]);
-
   return (
     <div className="relative flex flex-col h-full">
       {isLoading && (
